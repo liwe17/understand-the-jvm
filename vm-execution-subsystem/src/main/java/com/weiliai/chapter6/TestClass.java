49dde3f87b9f7a0cedb292410894163b52619680
liwei <18510247859@163.com>
2021-11-12 17:55:20 +0800

    chapter6 init

---
+++ vm-execution-subsystem/src/main/java/com/weiliai/chapter6/TestClass.java
@@ -0,0 +1,17 @@
+package com.weiliai.chapter6;
+
+/**
+ * 清单6-1
+ *
+ * @author LiWei
+ * @date 2021/11/12
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+
+}
